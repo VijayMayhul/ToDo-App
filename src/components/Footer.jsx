5e0dfab4a2f138b95cc60fc0958762825b188b1d
VijayMayhul <vijaymayhul@gmail.com>
2024-01-28 01:30:09 +0530

    Adding ToDo App Files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+  return (
+    <>
+    <footer class="py-5 mt-3 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">
+                Copyright <a href="mailTo:vijaymayhul@gmail.com">©vijaymayhul@gmail.com</a> 2024
+            </p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+  return (
+    <>
+        <header>
+            <div className='row'>
+            <h1 className='h1 text-center bg-dark text-capitalize text-warning py-3'>My ToDo</h1>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
